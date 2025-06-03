8ba55b24dd777979deee03d5bf711a0f8f99536e
kevinloi142 <48134251+kevinloi142@users.noreply.github.com>
2025-06-03 15:51:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,117 @@
+const axios = require('axios');
+const { writeFileSync } = require('fs');
+const { ZonedDateTime, ZoneId } = require('@js-joda/core');
+require('@js-joda/timezone');
+const key = 'IixDo0rkv3xMZQTGgCpnzcGsW2kldjAe';
+
+const getDate = () => {
+    const zdt = ZonedDateTime.now(ZoneId.of('America/Los_Angeles'));
+    const year = zdt.year().toString().padStart(2, '0');
+    const month = zdt.monthValue().toString().padStart(2, '0');
+    const day = zdt.dayOfMonth().toString().padStart(2, '0');
+    const hour = zdt.hour().toString().padStart(2, '0');
+    const minute = zdt.minute().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hour}:${minute}`;
+};
+
+const getData = async (lat, lng) => {
+    const url = 'https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute';
+    const zoom = 20;
+
+    const { data } = await axios({
+        method: 'get',
+        url: `${url}/${zoom}/json`,
+        params: {
+            key,
+            point: `${lat},${lng}`,
+            unit: 'mph'
+        }
+    });
+
+    data.flowSegmentData.point = { lat, lng };
+
+    delete data.flowSegmentData.coordinates;
+    delete data.flowSegmentData['@version'];
+
+    return data.flowSegmentData;
+};
+
+const getAllStreets = async (date) => {
+    return {
+        date,
+        highwayTwo: [
+            await getData(34.0261335,-118.476072), 
+            await getData(34.0261335,-118.476072),
+            await getData(34.0261335,-118.476072),
+            await getData(34.0256252,-118.4778303),
+            await getData(34.0256252,-118.4778303),
+            await getData(34.0278022,-118.4741866),
+            await getData(34.0349299,-118.463976),
+            await getData(34.0405474,-118.4474101),
+        ],
+        willshireBlvd: [
+           await getData(34.0175624,-118.4964529), 
+           await getData(34.0198166,-118.4942322), 
+           await getData(34.0198166,-118.4942322), 
+           await getData(34.0234562,-118.4886807), 
+           await getData(34.0284915,-118.4833412), 
+           await getData(34.0337953,-118.4777273), 
+           await getData(34.0472342,-118.4643287), 
+           await getData(34.0513275,-118.4589269), 
+        ],
+        highwayTen: [ 
+            await getData(34.0176184,-118.481321),
+            await getData(34.0176184,-118.481321),
+            await getData(34.0192055,-118.476708),
+            await getData(34.0260031,-118.4646028),
+            await getData(34.0296384,-118.4455725),
+        ],
+        fourthSt: [
+            await getData(34.0196919,-118.4907066),
+            await getData(34.0174191,-118.4949884),
+            await getData(34.0151769,-118.4928233),
+        ],
+        lincolnBlvd: [
+            await getData(34.021716,-118.4911839),
+            await getData(34.020946,-118.4917534),
+            await getData(34.0173022,-118.4889126),
+        ],
+        fourteenthSt: [
+            await getData(34.0254804,-118.4863837),
+            await getData(34.0254804,-118.486383),
+            await getData(34.0261248,-118.4870393),
+        ],
+        twentiethSt: [
+            await getData(34.0313657,-118.4814129),
+            await getData(34.0313657,-118.4814129),
+            await getData(34.0282196,-118.4786983),
+        ],
+        twentieSixthSt: [
+            await getData(34.035359,-118.4775441),
+            await getData(34.035359,-118.4775441),
+            await getData(34.0315788,-118.4726944),
+        ],
+        bundyDr: [
+            await getData(34.0430893,-118.463707),
+            await getData(34.0430893,-118.463707),
+            await getData(34.0353517,-118.4558876),
+        ],
+        barringtonAve: [
+            await getData(34.0498258,-118.4608534),
+            await getData(34.0456968,-118.4572626),
+            await getData(34.0414742,-118.4543709)
+        ]
+    };
+};
+
+
+(async () => {
+    const date = getDate();
+    const path = '/root/TomTomScript/output';
+
+    writeFileSync(
+	`${path}/${date}.json`,
+	JSON.stringify(await getAllStreets(date))
+    );
+})();
